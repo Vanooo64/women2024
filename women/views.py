00fd64876e01cc1be6337e8f392363af7e2b2004
Vano <glazglojon@gmail.com>
2024-08-14 16:24:53 +0300

    add sitewomen

--- women/views.py
+++ women/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def categories(request, cat_id):
     return HttpResponse(f'<h1>Статі по категоріям</h1><p>id: {cat_id}<p/>')
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Статі по категоріям</h1><p>slug: {cat_slug}<p/>')
