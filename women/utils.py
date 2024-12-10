fd651acc1ffa827a36be7303268e83775e825440
Vano <glazglojon@gmail.com>
2024-12-10 16:17:28 +0200

    урок 61. Шаблонні контекстні процесори

--- women/utils.py
+++ women/utils.py
@@ -1,7 +1,6 @@
 menu = [{'title': "Про сайт", 'url_name': 'about'},
         {'title': "Додати статью", 'url_name': 'add_page'},
         {'title': "Зворотній звязок", 'url_name': 'contact'},
-        {'title': "Увійти", 'url_name': 'login'},
         ]
 
 
@@ -18,11 +17,7 @@ class DataMixin:
         if self.cat_selected is not None:
             self.extra_context['cat_selected'] = self.cat_selected
 
-        if 'menu' not in self.extra_context:
-            self.extra_context['menu'] = menu
-
     def get_mixin_context(self, context, **kwargs):
-        context['menu'] = menu
         context['cat_selected'] = None
         context.update(kwargs)
         return context
